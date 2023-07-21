2e3159b9f17245bf5e5f224afbb0ea36a6855315
deltechdesigns <deltechdesigns@gmail.com>
2023-07-20 17:00:07 -0700

    still on the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
-import { useState } from 'react'; 
+import { useEffect, useState } from 'react'; 
+import {  } from "react-icons/io";
 import  './App.css'
 
 function App() {
+  const [advice, setAdvice] = useState(""); 
   const [count, setCount] = useState(0)
 
   async function getAdvice(){
     const res = await fetch("https://api.adviceslip.com/advice"); 
-    const data = res.json(); 
-    console.log(data);
+    const data = await res.json(); 
+    setAdvice(data.slip.advice);
+    setCount(count => count + 1)
   }
+
+  useEffect(function(){
+    getAdvice()
+  }, [])
 
   return (
     <>
       <div className='container'>
-          <h1>Hello World</h1>
+        <div className='inner-container'>
+          <p>icon</p>
+          <h1 className='title'>Get Random Advice</h1>
+          <div className="advice-text">{advice}</div>
           <button className='btn' onClick={getAdvice}>Get Advice</button>
+          <p className='count'>The number of piece of advice is {count}</p>
+        </div>
       </div>
     </>
   )
